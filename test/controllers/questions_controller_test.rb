ed6c12b02fdf20c6316d1571d9d7196be232b5d9
Marc Hadley <mhadley@mitre.org>
2016-09-29 14:46:40 -0400

    Fix tests to work with Devise

--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -1,21 +1,24 @@
 require 'test_helper'
 
 class QuestionsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
     @question = questions(:one)
+    sign_in users(:admin)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get questions_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_question_url
     assert_response :success
   end
 
-  test "should create question" do
+  test 'should create question' do
     assert_difference('Question.count') do
       post questions_url, params: { question: { author: @question.author, content: @question.content } }
     end
@@ -23,22 +26,22 @@ class QuestionsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to question_url(Question.last)
   end
 
-  test "should show question" do
+  test 'should show question' do
     get question_url(@question)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_question_url(@question)
     assert_response :success
   end
 
-  test "should update question" do
+  test 'should update question' do
     patch question_url(@question), params: { question: { author: @question.author, content: @question.content } }
     assert_redirected_to question_url(@question)
   end
 
-  test "should destroy question" do
+  test 'should destroy question' do
     assert_difference('Question.count', -1) do
       delete question_url(@question)
     end
