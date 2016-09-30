33701b5c5708cc5790700c69a3d7b680833a751d
Andrew Pellitieri <apellitieri@mitre.org>
2016-09-30 00:34:24 -0400

    Fixed all rubocop linter errors except hash rockets - need to discuss if this is necessary

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,3 @@
 class Question < ApplicationRecord
-  # resourcify # Need to check if this is necessary
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :questions
   devise_for :users
   root to: 'questions#index'
+
   # get 'questions' => 'questions#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
