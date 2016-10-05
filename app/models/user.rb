d65ac6fd08548bc2993449c23c8d88d414c6a05e
rdingwell <rob@robdingwell.org>
2016-10-05 10:07:34 -0400

    Revert "Revert 2 response set"

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  rolify
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+end
---
+++ db/migrate/20160830175151_add_admin_flag.rb
@@ -0,0 +1,5 @@
+class AddAdminFlag < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :admin, :boolean, default: false
+  end
+end
