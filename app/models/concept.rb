aaa757e51eb6f7243cde4d7363dda263a81a8f2b
Andrew Pellitieri <apellitieri@mitre.org>
2016-10-12 15:26:58 -0400

    functional linking with HABTM join table

--- app/models/concept.rb
+++ app/models/concept.rb
@@ -1,2 +1,7 @@
 class Concept < ApplicationRecord
+  # Just adding this until we confirm we want to replace these with an intermediate model
+  # instead of a join table
+  # rubocop:disable Rails/HasAndBelongsToMany
+  has_and_belongs_to_many :response_sets
+  # rubocop:enable Rails/HasAndBelongsToMany
 end
--- app/models/response_set.rb
+++ app/models/response_set.rb
@@ -1,4 +1,9 @@
 class ResponseSet < ApplicationRecord
   has_many :questions
   has_many :responses, dependent: :nullify
+  # Just adding this until we confirm we want to replace these with an intermediate model
+  # instead of a join table
+  # rubocop:disable Rails/HasAndBelongsToMany
+  has_and_belongs_to_many :concepts
+  # rubocop:enable Rails/HasAndBelongsToMany
 end
