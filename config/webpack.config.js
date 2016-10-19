29d5311061124383c80ee60f4aedd3b231f92435
Andy Gregorowicz <andy@mitre.org>
2016-10-19 11:42:33 -0400

    Replacing the rails asset pipeline with webpack
    The application should now be run with foreman. You can do so with the following
    command:
    
        foreman start -p 3000
    
    The rails asset pipeline has been disabled and all ruby gems used to manage
    JavaScript libraries have been removed from the project's Gemfile.
    
    JavaScript and CSS are now managed by webpack and development of these should
    happen in the webpack folder.
    
    Note: The webpack server should be running for the test suite to pass.

---
+++ config/webpack.config.js
@@ -0,0 +1,88 @@
+// Example webpack configuration with asset fingerprinting in production.
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var StatsPlugin = require('stats-webpack-plugin');
+var autoprefixer = require('autoprefixer');
+
+// must match config.webpack.dev_server.port
+var devServerPort = 3808;
+
+// set NODE_ENV=production on the environment to add asset fingerprints
+var production = process.env.NODE_ENV === 'production';
+
+var config = {
+  entry: {
+    // Sources are expected to live in $app_root/webpack
+    'application': './webpack/application.js',
+    'bootstrap': 'bootstrap-loader'
+  },
+
+  output: {
+    // Build assets directly in to public/webpack/, let webpack know
+    // that all webpacked assets start with webpack/
+
+    // must match config.webpack.output_dir
+    path: path.join(__dirname, '..', 'public', 'webpack'),
+    publicPath: '/webpack/',
+
+    filename: production ? '[name]-[chunkhash].js' : '[name].js'
+  },
+
+  postcss: [ autoprefixer({ browsers: ['last 2 versions'] }) ],
+
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
+      { test: /\.css$/, loaders: ['style', 'css', 'postcss'] },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff&name=assets/[name].[ext]' },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader?name=assets/[name].[ext]' },
+      { test: /\.scss$/, loaders: ['style', 'css', 'postcss', 'sass'] }
+    ]
+  },
+
+  resolve: {
+    root: path.join(__dirname, '..', 'webpack')
+  },
+
+  plugins: [
+    // must match config.webpack.manifest_filename
+    new StatsPlugin('manifest.json', {
+      // We only need assetsByChunkName
+      chunkModules: false,
+      source: false,
+      chunks: false,
+      modules: false,
+      assets: true
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    })]
+};
+
+if (production) {
+  config.plugins.push(
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: { warnings: false },
+      sourceMap: false
+    }),
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify('production') }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin()
+  );
+} else {
+  config.devServer = {
+    port: devServerPort,
+    headers: { 'Access-Control-Allow-Origin': '*' }
+  };
+  config.output.publicPath = '//localhost:' + devServerPort + '/webpack/';
+  // Source maps
+  config.devtool = 'cheap-module-eval-source-map';
+}
+
+module.exports = config;
