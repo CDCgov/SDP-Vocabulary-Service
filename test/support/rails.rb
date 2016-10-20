d2aa1168deb19ee94d40771938ae9d06282ef2ce
Rob Dingwell <rob@robdingwell.org>
2016-10-20 16:07:34 -0400

    adding support files for test helper tpo separate helper concerns. Adding rake tasks to support additional testing from within the rake environment.  Adding in configuration files for cucumber and brakeman.  Updating .gitignore file to deal with generated report locations

---
+++ test/support/rails.rb
@@ -0,0 +1,8 @@
+require 'rails/test_help'
+module ActiveSupport
+  class TestCase
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    fixtures :all
+    # Add more helper methods to be used by all tests here...
+  end
+end
