e6b15e869f018a0e4c12b1a2a1dd515b602ec9e0
Andrew Pellitieri <apellitieri@mitre.org>
2016-10-28 16:47:31 -0400

    Fixed javascript that was breaking cucumber tests

--- webpack/forms.js
+++ webpack/forms.js
@@ -2,17 +2,16 @@ const $ = require('jquery');
 
 exports.addQuestion = function addQuestion(questionName, question, responseSets) {
   const tbl = $('#questionTable > tbody:last-child')
-  let responseSetsSelect = "<select name='response_set_ids[]'>";
+  var responseSetsSelect = "<select name='response_set_ids[]'>";
   responseSetsSelect += "<option label=' '></option>";
-  responseSets.forEach((rs) => {
-    responseSetsSelect += `<option value="${rs.id}">${rs.name}</option>`;
+  responseSets.forEach(function(rs){
+    responseSetsSelect += '<option value="'
+    responseSetsSelect += rs.id
+    responseSetsSelect += '">'
+    responseSetsSelect += rs.name
+    responseSetsSelect += '</option>';
   });
-  responseSetsSelect << "</select>";
-  tbl.append(`<tr>
-    <td>${questionName}</td>
-    <input type="hidden" name="question_ids[]" value="${question}"/>
-    <td>
-        ${responseSetsSelect}
-    </td>
-  </tr>`);
+  responseSetsSelect += "</select>";
+  var appendString = '<tr><td>' + questionName + '</td><input type="hidden" name="question_ids[]" value="' + question + '"/><td>' + responseSetsSelect + '</td></tr>'
+  tbl.append(appendString);
 }
