bf592084bbeae0a803a4fd39c6aaeb0e3ab5c686
Rob Dingwell <rob@robdingwell.org>
2016-11-01 10:50:40 -0400

    The application uses webpack for it's javascript dependency management system. Webpack is reliant on the node package manager for pulling dependencies down to be used by the application. the standard ruby sti builder image does not have the ability to perform the npm install task to pull the javascript packages down for use. Additionally,  The ruby sti builder has the ability to precompile assets for use in the production environment but does not support precompiling of webpack resources. This commit address both of those issues by calling out to the original sti assemble script to perform the base rails application setup and then configures npmm performs the npm install and then builds the precompiled assets for use i nthe production environment.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
+  config.public_file_server.enabled = true
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
