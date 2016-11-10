a0c5b4e2dd502f468f3ad6567c23aea7c3c77939
Andrew Schreiber <schreiaj@gmail.com>
2016-11-10 14:17:52 -0500

    Add previous versions to Question.show and don't pull forward columns that Andy didnt

--- app/models/question.rb
+++ app/models/question.rb
@@ -33,7 +33,7 @@ class Question < ApplicationRecord
   end
 
   def build_new_revision
-    new_revision = Question.new(content: content, created_at: created_at, updated_at: updated_at, created_by: created_by,
+    new_revision = Question.new(content: content,
                                 version_independent_id: version_independent_id,
                                 version: version + 1, question_response_sets: question_response_sets,
                                 response_sets: response_sets, form_questions: form_questions, forms: forms,
