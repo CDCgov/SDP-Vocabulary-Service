c7b2b5ee1383221ef20ac1cc09a4f5f9bf041f5b
Rob Dingwell <rob@robdingwell.org>
2016-11-22 12:00:07 -0500

    initial reactification of question widget.

---
+++ webpack/components/QuestionWidget.js
@@ -0,0 +1,58 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class QuestionWidget extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="question-group" id={"question_id_"+this.props.question.id}>
+          <div className="panel panel-default">
+              <div className="question-container">
+
+                  <ul className="list-inline">
+                      <li><a href={SDP.routes.question_path(this.props.question)}>{this.props.question.content}</a></li>
+                      <li className="pull-right">
+                          <a>
+                              <span className="glyphicon glyphicon-signal"></span>
+                          </a>
+                      </li>
+                  </ul>
+              </div>
+
+              <div className="response-set-details">
+                  <ul className="list-inline">
+                      <li className="reponse-number panel-toggle" data-toggle="collapse" href={"#collapse"+this.props.question.id}>
+                          <span className="glyphicon glyphicon-th-list"></span></li>
+                      <li className="pull-right question-menu">
+                          <div className="dropdown">
+                              <a id={"question_"+this.props.question.id+"_menu"} className="dropdown-toggle" type="" data-toggle="dropdown">
+                                  <span className="glyphicon glyphicon-option-horizontal"></span>
+                              </a>
+                              <ul className="dropdown-menu">
+                              <li>
+                                <a href={SDP.routes.revise_question_path(this.props.question)}>Revise</a>
+                              </li>
+                              <li>
+                                <a href={SDP.routes.question_path(this.props.question)}>Details</a>
+                              </li>
+                                  <li>
+                                  <a data-confirm="Are you sure?" rel="nofollow" data-method="delete" href={SDP.routes.question_path(this.props.question)}>Delete</a>
+                                  </li>
+                              </ul>
+                          </div>
+                      </li>
+                  </ul>
+              </div>
+
+              <div className="panel-collapse panel-details collapse" id={"collapse" + this.props.question.id}>
+                  <div className="panel-body">
+                  </div>
+              </div>
+
+          </div>
+      </div>
+    );
+  }
+}
