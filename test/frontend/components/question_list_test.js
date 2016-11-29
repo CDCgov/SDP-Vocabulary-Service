705384e17b1a35e4772f1805e99eb04e4b226cc0
Andy Gregorowicz <andy@mitre.org>
2016-11-29 15:44:11 -0500

    Refactored the QuestionList component
    Removed unnecessary constructors
    Now accessing route helpers as React props
    Simplified test code since it doesn't need access to the component
    instance
    Reformatted JSX to 2 space indenting

--- test/frontend/components/question_list_test.js
+++ test/frontend/components/question_list_test.js
@@ -1,21 +1,15 @@
-import _$ from 'jquery';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
-import { expect } from '../test_helper';
+import { expect, renderComponent } from '../test_helper';
 import QuestionList from '../../../webpack/components/QuestionList';
-
-const $ = _$(window);
+import routes from '../mock_routes';
 
 describe('QuestionList', () => {
-  let component, componentInstance;
+  let component;
 
   beforeEach(() => {
     const questions = [{id: 1, content: "Is this a question?", question_type: ""},
                       {id: 2, content: "Whats your name", question_type: ""},
-                      {id: 3, content: "What is a question?", question_type: ""}]
-    componentInstance =  TestUtils.renderIntoDocument(<QuestionList questions={questions}/>);
-    component = $(ReactDOM.findDOMNode(componentInstance));
+                      {id: 3, content: "What is a question?", question_type: ""}];
+    component = renderComponent(QuestionList, {questions, routes});
   });
 
   it('should create list of questions', () => {
--- webpack/components/QuestionList.js
+++ webpack/components/QuestionList.js
@@ -1,26 +1,23 @@
 import React, { Component, PropTypes } from 'react';
 import QuestionWidget from './QuestionWidget';
-export default class QuestionList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {questions: props.questions};
-  }
 
+export default class QuestionList extends Component {
   render() {
     return (
       <div className="question-group">
-        {this.state.questions.map((function(q) {
+        {this.props.questions.map((q) => {
           // Each List Item Component needs a key attribute for uniqueness:
           // http://facebook.github.io/react/docs/multiple-components.html#dynamic-children
           // In addition, we pass in our item data and a handleOnClick function that executes a callback that passes
           // the item value
-          return <QuestionWidget key={q.id} question={q} />;
-        }))}
+          return <QuestionWidget key={q.id} question={q} routes={this.props.routes} />;
+        })}
       </div>
     );
   }
 }
 
 QuestionList.propTypes = {
-  questions: PropTypes.arrayOf(QuestionWidget.propTypes.question)
+  questions: PropTypes.arrayOf(QuestionWidget.propTypes.question).isRequired,
+  routes: QuestionWidget.propTypes.routes.isRequired
 };
