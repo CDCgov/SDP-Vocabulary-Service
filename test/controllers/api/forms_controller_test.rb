34cffc1ab37454cd6968ca9aa286b825e9ab3719
Andrew Hubley <ahubley@mitre.org>
2016-12-01 14:41:51 -0500

    Added json_schema gem for schema testing serializers
    Added actual json schema files to test serializers against
    
    Added calls to assert_response_schema for serialization testing:

--- test/controllers/api/forms_controller_test.rb
+++ test/controllers/api/forms_controller_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class FormsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
+  include ActiveModelSerializers::Test::Schema
+  include ActiveModelSerializers::Test::Serializer
 
   setup do
     @form = forms(:one)
@@ -11,5 +13,7 @@ class FormsControllerTest < ActionDispatch::IntegrationTest
   test 'api should show form' do
     get api_form_url(@form)
     assert_response :success
+    assert_serializer 'FormSerializer'
+    assert_response_schema('forms/show.json')
   end
 end
