ff304ec6bf923636e878056da74a33137fedb999
Rob Dingwell <rob@robdingwell.org>
2016-12-01 15:03:01 -0500

    adding tests for response type model

--- test/models/response_type_test.rb
+++ test/models/response_type_test.rb
@@ -1,4 +1,28 @@
 require 'test_helper'
 
 class ResponseTypeTest < ActiveSupport::TestCase
+  test 'should require a name' do
+    type = ResponseType.new
+    assert_not type.valid?
+  end
+
+  test 'should save with a name' do
+    type = ResponseType.new(name: 'ResponseType')
+    assert type.valid?
+    assert type.save, 'Should have saved to database'
+  end
+
+  test 'should require name uniqueness ' do
+    type = ResponseType.new(name: 'ResponseType')
+    assert type.valid?
+    assert type.save, 'Should have saved to database'
+
+    type2 = ResponseType.new(name: 'ResponseType')
+    assert_not type2.valid?
+  end
+
+  test 'should have a name' do
+    type = ResponseType.new(name: 'QuestionType')
+    assert_equal type.name, 'QuestionType'
+  end
 end
