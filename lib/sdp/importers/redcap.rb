b4e5652afbab6056a0b0b4d34e7f742faa073787
Rob Dingwell <rob@robdingwell.org>
2016-12-20 15:54:58 -0500

    [WIP] added simple import of redcap forms and questions with very simplistic tests.  fixed bug in forms show.html.erb file that did not properly show questions with mapped response sets

--- lib/sdp/importers/redcap.rb
+++ lib/sdp/importers/redcap.rb
@@ -4,31 +4,81 @@ module SDP
       attr_accessor :xml
       attr_accessor :user
 
-      def intialize(xml, user)
+      def initialize(xml, user)
         @xml = parse_document(xml)
         @user = user
       end
 
       def import
+        parse.each_pair do |_k, v|
+          v.save
+        end
       end
 
-      def import_forms
+      def parse
+        response_sets = parse_response_sets
+        questions = parse_questions(response_sets)
+        item_groups = parse_item_groups(questions)
+        forms = parse_forms(item_groups)
+        forms
       end
 
-      def import_form_questions
+      def parse_forms(item_groups)
+        forms = {}
+        xml.xpath('//odm:FormDef').each do |f|
+          form = Form.new(name: f['Name'],
+                          created_by: user)
+          forms[f['OID']] = form
+          f.xpath('./odm:ItemGroupRef').each do |igr|
+            ig_questions = item_groups[igr['ItemGroupOID']]
+            ig_questions.each do |q|
+              form.form_questions << FormQuestion.new(question: q, response_set: q.response_sets[0])
+            end
+          end
+        end
+        forms
       end
 
-      def import_response_sets
+      def parse_item_groups(questions)
+        item_groups = {}
+        xml.xpath('//odm:ItemGroupDef').each do |igd|
+          item_groups[igd['OID']] = []
+          igd.xpath('./odm:ItemRef').each do |ir|
+            item_groups[igd['OID']] << questions[ir['ItemOID']]
+          end
+        end
+        item_groups
       end
 
-      def import_questions
+      def parse_response_sets
+        code_lists = {}
+        xml.xpath('//odm:CodeList').each do |cl_element|
+          cl = ResponseSet.new(name: cl_element['Name'], created_by: user)
+          code_lists[cl_element['OID']] = cl
+          cl_element.xpath('./odm:CodeListItem').each do |item|
+            value = item['CodedValue']
+            display_name = item.at_xpath('./odm:Decode/odm:TranslatedText').text
+            cl.responses << Response.new(value: (value || display_name),
+                                         display_name: (display_name || value))
+          end
+        end
+        code_lists
       end
 
-      def import_responses
+      def parse_questions(response_sets)
+        questions = {}
+        xml.xpath('//odm:ItemDef').each do |q|
+          question = Question.new(content: q.at_xpath('./odm:Question/odm:TranslatedText').text,
+                                  created_by: user)
+          questions[q['OID']] = question
+          clr = q.at_xpath('./odm:CodeListRef')
+          question.response_sets << response_sets[clr['CodeListOID']] if clr
+        end
+        questions
       end
 
-      def parse_document(xml)
-        doc = Nokogiri::XML(xml)
+      def parse_document(file)
+        doc = Nokogiri::XML(file)
         doc.root.add_namespace_definition('odm', 'http://www.cdisc.org/ns/odm/v1.3')
         doc.root.add_namespace_definition('ds', 'http://www.w3.org/2000/09/xmldsig#')
         doc.root.add_namespace_definition('xsi', 'http://www.w3.org/2001/XMLSchema-instance')
---
+++ test/unit/lib/redcap_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+require 'sdp/importers/redcap'
+
+class Redcap < ActiveSupport::TestCase
+  # Essentially testing that Versionable is working in Form
+  test 'parse_redcap_file' do
+    u = users(:admin)
+    f = File.new('./test/fixtures/files/redcap.xml')
+    importer = SDP::Importers::Redcap.new(f, u)
+    rs = importer.parse_response_sets
+    assert_equal 67, rs.values.length
+    questions = importer.parse_questions(rs)
+    assert_equal  198, questions.values.length
+    item_groups = importer.parse_item_groups(questions)
+    assert_equal 16, item_groups.values.length
+    forms = importer.parse_forms(item_groups)
+    assert_equal 5, forms.values.length
+
+    rscount = ResponseSet.count
+    qcount = Question.count
+    formcount = Form.count
+
+    forms.each_pair do |_k, v|
+      v.save
+    end
+
+    assert_equal rscount + 67, ResponseSet.count
+    assert_equal qcount + 198, Question.count
+    assert_equal formcount + 5, Form.count
+  end
+end
