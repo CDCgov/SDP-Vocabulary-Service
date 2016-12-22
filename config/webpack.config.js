1767a6991ba8fb59de49e8425facc43e85b4c02c
Kevin Hennessy <khennessy@mitre.org>
2016-12-21 19:06:02 -0500

    Fixed a minor webpack error

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -20,7 +20,7 @@ var config = {
     'application': './webpack/application.js',
     'response_set': './webpack/response_set.js',
     'forms': './webpack/forms.js',
-	'form_list': './webpack/form_list.js'
+    'form_list': './webpack/form_list.js',
     'question_list': './webpack/question_list.js',
     'response_set_list': './webpack/response_set_list.js',
     'search': './webpack/search.js',
