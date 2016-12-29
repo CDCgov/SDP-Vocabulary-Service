44326d9816258fecf512cb5770faa20c4acff41f
Kevin Hennessy <khennessy@mitre.org>
2016-12-29 12:28:05 -0500

    Added some mocha tests

--- test/frontend/mock_routes.js
+++ test/frontend/mock_routes.js
@@ -1,2 +1,4 @@
 exports.questionPath = (q) => `/questions/${q.id}`;
 exports.reviseQuestionPath = (q) => `/questions/${q.id}/revise`;
+exports.formPath = (f) => `/forms/${f.id}/revise`;
+exports.reviseFormPath = (f) => `/forms/${f.id}/revise`;
