5045609c6b6e2bb672ce19f04e89c7abc8a1cbba
Kevin Hennessy <khennessy@mitre.org>
2017-01-10 16:29:38 -0500

    Streamlining forms, mystuff for speed

--- webpack/form_list.js
+++ webpack/form_list.js
@@ -2,16 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import FormList from './components/FormList';
 import Routes from './routes';
-import { camelCase } from './camelcase';
 
 const forms = JSON.parse(document.getElementById('forms-json').innerHTML);
 
-forms.forEach((form) => {
-  Object.keys(form).forEach((k) => {
-    if (k.includes('_')) {
-      form[camelCase(k)] = form[k];
-      delete form[k];
-    }
-  });
-});
 ReactDOM.render(<FormList forms={forms} routes={Routes} />, document.getElementById('form_list'));
