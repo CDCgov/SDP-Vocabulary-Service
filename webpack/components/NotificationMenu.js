5ddfa18ce3a424dd32c606c87780e3727885df65
Andrew Pellitieri <apellitieri@mitre.org>
2017-01-11 12:27:33 -0500

    Commiting eslint fixes so I can rebase before implementing tests

--- webpack/components/NotificationMenu.js
+++ webpack/components/NotificationMenu.js
@@ -1,37 +1,23 @@
 import React, { Component, PropTypes } from 'react';
 import Notification from './Notification';
-import routes from '../routes';
-import axios from 'axios';
 import { readNotification } from '../actions/notification';
-
-function getCSRFToken() {
-  const metas = document.getElementsByTagName('meta');
-  for (let i = 0; i < metas.length; i++) {
-    const meta = metas[i];
-    if (meta.getAttribute('name') === 'csrf-token') {
-      return meta.getAttribute('content');
-    }
-  }
-  return null;
-}
 
 export default class NotificationMenu extends Component {
   constructor(props){
     super(props);
 
     this.state = {
-      notifications: props.notifactions
+      notifications: props.notifications
     };
   }
 
   notificationClick(id, url) {
     readNotification(id);
     // Redirect to the url in notification:
-    console.log(url.includes(window.location.pathname));
     if (url.includes(window.location.pathname)) {
       window.location.reload();
     }
-    window.location.hash = ''
+    window.location.hash = '';
     window.location = url;
   }
 
