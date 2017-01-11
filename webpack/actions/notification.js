efae5e0501dd202f3ddc0c8ef69a7e454dbdf904
Andrew Pellitieri <apellitieri@mitre.org>
2017-01-11 16:41:51 -0500

    Requested changes to remove redux placeholders

--- webpack/actions/notification.js
+++ webpack/actions/notification.js
@@ -1,15 +1,11 @@
 import axios from 'axios';
 import routes from '../routes';
-import { READ_NOTIFICATION } from './types';
 
 export function readNotification(notificationIds) {
-  return {
-    type: READ_NOTIFICATION,
-    payload: axios.post(routes.notifications_mark_read_path(), {
-      authenticityToken: getCSRFToken(),
-      ids: [notificationIds]
-    })
-  };
+  axios.post(routes.notifications_mark_read_path(), {
+    authenticityToken: getCSRFToken(),
+    ids: [notificationIds]
+  });
 }
 
 function getCSRFToken() {
