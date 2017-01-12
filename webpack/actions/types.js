3b0dee24001aaaf3455854452e06908a32392085
Rob Dingwell <rob@robdingwell.org>
2017-01-12 18:22:21 -0500

    rebasing from development

--- webpack/actions/types.js
+++ webpack/actions/types.js
@@ -1,12 +1,10 @@
 // Question types
 export const ADD_QUESTION = 'ADD_QUESTION';
 export const REMOVE_QUESTION = 'REMOVE_QUESTION';
-<<<<<<< HEAD
 export const REORDER_QUESTION = 'REORDER_QUESTION';
-=======
 export const FETCH_QUESTIONS = 'FETCH_QUESTION';
 export const FETCH_QUESTIONS_FULFILLED = 'FETCH_QUESTION_FULFILLED';
->>>>>>> Turning Questions index page into react components and making search nicer
+
 // Comment types
 export const ADD_COMMENT = 'ADD_COMMENT';
 export const ADD_COMMENT_FULFILLED = "ADD_COMMENT_FULFILLED";
