29d87af2bd1f31e9d40ed21d47584b38cb91d7ad
Andy Gregorowicz <andy@mitre.org>
2017-01-18 09:08:31 -0500

    [WIP] Starting SPA conversion
    Moving the application to a single page application. This creates a
    landing controller to replace the dashboard controller. Sets up
    react-router and starts to lay out some of the high level redux
    containers.

---
+++ webpack/containers/FormsContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class FormsContainer extends Component {
+  render() {
+    return (
+      <p>Place holder</p>
+    );
+  }
+}
+
+export default FormsContainer;
