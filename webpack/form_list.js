d24ac54bf32f33ef371f0267ae29e5d08b7163c5
Kevin Hennessy <khennessy@mitre.org>
2017-01-19 13:25:47 -0500

    Small fix to keep older cucumber tests passing

--- webpack/form_list.js
+++ webpack/form_list.js
@@ -5,4 +5,8 @@ import Routes from './routes';
 
 const forms = JSON.parse(document.getElementById('forms-json').innerHTML);
 
+forms.map((aForm) => {
+  aForm.userId = aForm.createdBy.email;
+});
+
 ReactDOM.render(<FormList forms={forms} routes={Routes} />, document.getElementById('form_list'));
