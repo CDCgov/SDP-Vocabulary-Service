8c23bd913ada9c4b58ae723b226251fd604030ae
Andy Gregorowicz <andy@mitre.org>
2017-01-19 10:08:29 -0500

    [WIP] Stubbing out routes

---
+++ webpack/containers/QuestionsContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class QuestionsContainer extends Component {
+  render() {
+    return (
+      <p>Questions Place holder</p>
+    );
+  }
+}
+
+export default QuestionsContainer;
---
+++ webpack/containers/ResponseSetsContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ResponseSetsContainer extends Component {
+  render() {
+    return (
+      <p>Response Sets Place holder</p>
+    );
+  }
+}
+
+export default ResponseSetsContainer;
