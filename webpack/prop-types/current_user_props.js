c44f861cb0ed4bd60db77dad4b752def95852e0a
Andy Gregorowicz <andy@mitre.org>
2017-01-19 11:30:46 -0500

    Pulling prop types out into their own files

---
+++ webpack/prop-types/current_user_props.js
@@ -0,0 +1,11 @@
+import { PropTypes } from 'react';
+
+const currentUserProps = PropTypes.shape({
+  id:        PropTypes.number,
+  admin:     PropTypes.boolean,
+  email:     PropTypes.string,
+  firstName: PropTypes.string,
+  lastName:  PropTypes.string
+});
+
+export default currentUserProps;
