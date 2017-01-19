8972b91ed76674836ee4676794810e7d5bf4ec5f
Andrew Pellitieri <apellitieri@mitre.org>
2017-01-19 11:11:33 -0500

    Added new ResponseSetContainer based on new scheme

--- webpack/reducers/index.js
+++ webpack/reducers/index.js
@@ -5,9 +5,10 @@ import {
 import questions from './questions';
 import comments from './comments';
 import stats from './stats';
+import responseSets from './response_sets_reducer';
 
 const rootReducer = combineReducers({
-  questions, comments, stats
+  questions, comments, stats, responseSets
 });
 
 export default rootReducer;
