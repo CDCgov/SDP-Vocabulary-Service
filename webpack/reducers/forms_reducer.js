40a097c1a7ce5458e7facaa81c25ed69aa98a1f0
Kevin Hennessy <khennessy@mitre.org>
2017-01-20 10:01:35 -0500

    Array to Object

--- webpack/reducers/forms_reducer.js
+++ webpack/reducers/forms_reducer.js
@@ -1,12 +1,13 @@
 import {
   FETCH_FORMS_FULFILLED
 } from '../actions/types';
+import _ from 'lodash';
 
-const initialState = null;
+const initialState = {'loading': true};
 
 export default function forms(state = initialState, action) {
   if (action.type == FETCH_FORMS_FULFILLED) {
-    return [...action.payload.data];
+    return _.keyBy(action.payload.data, 'id');
   }
   return state;
 }
