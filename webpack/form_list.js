17df751571995e3664721f24dbd1ace394dbb0cb
Kevin Hennessy <khennessy@mitre.org>
2017-01-20 17:35:00 -0500

    Cuke fixes

--- webpack/form_list.js
+++ webpack/form_list.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import FormList from './components/FormList';
 import Routes from './routes';
+import _ from 'lodash';
 
 const forms = JSON.parse(document.getElementById('forms-json').innerHTML);
 
@@ -9,4 +10,4 @@ forms.map((aForm) => {
   aForm.userId = aForm.createdBy.email;
 });
 
-ReactDOM.render(<FormList forms={forms} routes={Routes} />, document.getElementById('form_list'));
+ReactDOM.render(<FormList forms={_.keyBy(forms,'id')} routes={Routes} />, document.getElementById('form_list'));
