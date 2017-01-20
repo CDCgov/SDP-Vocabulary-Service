7be8fa0c0de5acd76e9c873e5f9fd75edc4b3fec
Andy Gregorowicz <andy@mitre.org>
2017-01-20 10:44:42 -0500

    Fixed places that use ResponseSetList
    The responseSets property changed from an array to a object keyed
    by the id of the response set.

--- webpack/response_set_list.js
+++ webpack/response_set_list.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import ResponseSetList from './components/ResponseSetList';
 import Routes from "./routes";
 
+import _ from 'lodash';
+
 const responseSets = JSON.parse(document.getElementById('response-set-json').innerHTML);
-ReactDOM.render(<ResponseSetList responseSets={responseSets} routes={Routes} />, document.getElementById('response_set_list'));
+ReactDOM.render(<ResponseSetList responseSets={_.keyBy(responseSets, 'id')} routes={Routes} />, document.getElementById('response_set_list'));
