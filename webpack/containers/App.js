e70e2b100da05e3a9a70966be107cfd1e5c4b2a5
Andrew Schreiber <schreiaj@gmail.com>
2017-01-23 10:30:25 -0500

    Hooked up current user

--- webpack/containers/App.js
+++ webpack/containers/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header />
+        <Header currentUser={this.props.currentUser}/>
         {this.props.children}
         <footer className="footer">
           <div className="container">
