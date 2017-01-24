8962682da6e8974a7f031764ca0b686ef131751e
Andrew Pellitieri <apellitieri@mitre.org>
2017-01-24 09:24:34 -0500

    Basic working show page functionality in SPA

--- webpack/actions/response_set_actions.js
+++ webpack/actions/response_set_actions.js
@@ -1,13 +1,23 @@
 import axios from 'axios';
 import routes from '../routes';
 import {
-  FETCH_RESPONSE_SETS
+  FETCH_RESPONSE_SETS,
+  FETCH_RESPONSE_SET
 } from './types';
 
 export function fetchResponseSets() {
   return {
     type: FETCH_RESPONSE_SETS,
     payload: axios.get(routes.responseSetsPath(), {
+      headers: {'Accept': 'application/json', 'X-Key-Inflection': 'camel'}
+    })
+  };
+}
+
+export function fetchResponseSet(id) {
+  return {
+    type: FETCH_RESPONSE_SET,
+    payload: axios.get(routes.responseSetPath(id), {
       headers: {'Accept': 'application/json', 'X-Key-Inflection': 'camel'}
     })
   };
