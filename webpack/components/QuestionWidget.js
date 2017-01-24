26cd240d2a6d39a732381badeecd7b77c3fafb1e
jmazella <jmazella@mitre.org>
2017-01-24 14:20:49 -0500

    Fixing delete question functionality on question index page

--- webpack/components/QuestionWidget.js
+++ webpack/components/QuestionWidget.js
@@ -1,11 +1,51 @@
 import React, { Component, PropTypes } from 'react';
-import { questionProps } from "../prop-types/question_props";
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { questionProps }  from "../prop-types/question_props";
+import { deleteQuestion } from "../actions/questions_actions";
+import currentUserProps from "../prop-types/current_user_props";
 
-export default class QuestionWidget extends Component {
+class QuestionWidget extends Component {
+  constructor(props){
+    super(props);
+    this.deleteQuestionClick = this.deleteQuestionClick.bind(this);
+  }
+
+  deleteQuestionClick(){
+    if(confirm('Are you sure?')){
+      this.props.deleteQuestion(this.props.question.id, document.head.querySelector("[name=csrf-token]").content);
+    }
+  }
+
+  dropdownMenu(){
+    if(this.props.currentUser && this.props.currentUser.id){
+      return (
+        <ul className="dropdown-menu">
+          <li>
+            <a href={this.props.routes.reviseQuestionPath(this.props.question)}>Revise</a>
+          </li>
+          <li>
+            <a href={this.props.routes.questionPath(this.props.question)}>Details</a>
+          </li>
+          <li>
+            <span role='button' rel="nofollow" onClick={this.deleteQuestionClick}>Delete</span>
+          </li>
+        </ul>
+      );
+    }else{
+      return (
+        <ul className="dropdown-menu">
+          <li>
+            <a href={this.props.routes.questionPath(this.props.question)}>Details</a>
+          </li>
+        </ul>
+      );
+    }
+  }
 
   render() {
     return (
-      <div className="question-group" id={"question_id_"+this.props.question.id}>
+      <div className="question-group" id={`question_id_${this.props.question.id}`}>
         <div className="panel panel-default">
           <div className="question-container">
             <ul className="list-inline">
@@ -17,7 +57,6 @@ export default class QuestionWidget extends Component {
               </li>
             </ul>
           </div>
-
           <div className="response-set-details">
             <ul className="list-inline">
               <li className="reponse-number panel-toggle" data-toggle="collapse" href={`#collapse${this.props.question.id}`}>
@@ -28,37 +67,39 @@ export default class QuestionWidget extends Component {
                   <a id={`question_${this.props.question.id}_menu`} className="dropdown-toggle" type="" data-toggle="dropdown">
                     <span className="fa fa-ellipsis-h"></span>
                   </a>
-                  <ul className="dropdown-menu">
-                    <li>
-                      <a href={this.props.routes.reviseQuestionPath(this.props.question)}>Revise</a>
-                    </li>
-                    <li>
-                      <a href={this.props.routes.questionPath(this.props.question)}>Details</a>
-                    </li>
-                    <li>
-                      <a data-confirm="Are you sure?" rel="nofollow" data-method="delete" href={this.props.routes.questionPath(this.props.question)}>Delete</a>
-                    </li>
-                  </ul>
+                  {this.dropdownMenu()}
                 </div>
               </li>
             </ul>
           </div>
-
-          <div className="panel-collapse panel-details collapse" id={"collapse" + this.props.question.id}>
+          <div className="panel-collapse panel-details collapse" id={`collapse${this.props.question.id}`}>
             <div className="panel-body">
             </div>
           </div>
-
         </div>
       </div>
     );
   }
 }
 
+function mapStateToProps(state) {
+  return {
+    currentUser: state.currentUser
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({deleteQuestion}, dispatch);
+}
+
 QuestionWidget.propTypes = {
   question: questionProps,
+  currentUser: currentUserProps,
+  deleteQuestion: PropTypes.func,
   routes: PropTypes.shape({
     questionPath: PropTypes.func.isRequired,
     reviseQuestionPath: PropTypes.func.isRequired
   })
 };
+
+export default connect(mapStateToProps, mapDispatchToProps)(QuestionWidget);
