9ccb0a74187291c276e59bc12cfa1c4bd854503c
Andrew Pellitieri <apellitieri@mitre.org>
2017-01-24 09:24:34 -0500

    Basic working show page functionality in SPA

--- webpack/reducers/response_sets_reducer.js
+++ webpack/reducers/response_sets_reducer.js
@@ -1,7 +1,8 @@
 import _ from 'lodash';
 
 import {
-  FETCH_RESPONSE_SETS_FULFILLED
+  FETCH_RESPONSE_SETS_FULFILLED,
+  FETCH_RESPONSE_SET_FULFILLED
 } from '../actions/types';
 
 const initialState = null;
@@ -10,6 +11,10 @@ export default function responseSets(state = initialState, action) {
   switch (action.type) {
     case FETCH_RESPONSE_SETS_FULFILLED:
       return _.keyBy(action.payload.data, 'id');
+    case FETCH_RESPONSE_SET_FULFILLED:
+      const responseSetClone = Object.assign({}, state);
+      responseSetClone[action.payload.data.id] = action.payload.data;
+      return responseSetClone;
     default:
       return state;
   }
