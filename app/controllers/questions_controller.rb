42b9b98f31a8b39f9f0e25bfc5010611e34a0960
jmazella <jmazella@mitre.org>
2017-01-24 14:20:49 -0500

    Fixing delete question functionality on question index page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -87,10 +87,7 @@ class QuestionsController < ApplicationController
   # DELETE /questions/1.json
   def destroy
     @question.destroy
-    respond_to do |format|
-      format.html { redirect_to questions_url, notice: 'Question was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    render json: @question
   end
 
   private
