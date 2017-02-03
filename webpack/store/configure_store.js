f2f7ff675e70160d8095a96990963ef69a45fa38
jmazella <jmazella@mitre.org>
2017-02-03 10:26:33 -0500

    Type

--- webpack/store/configure_store.js
+++ webpack/store/configure_store.js
@@ -18,7 +18,7 @@ export default function configureStore(initialState) {
     questionsFromResponseSets,
     questionsFromForms,
     responseSetsFromQuestions,
-    parentFromResponseSets
+    parentFromResponseSets,
     responseTypesFromQuestions,
     questionTypesFromQuestions
   );
