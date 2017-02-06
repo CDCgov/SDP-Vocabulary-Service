a8b958dc61aa3bf71da46214521721e94960b3ce
Rob Dingwell <rob@robdingwell.org>
2017-02-06 10:36:48 -0500

    adding after hooks for form questions to update questions and responses sets after a form is saved or deleted

--- app/models/form_question.rb
+++ app/models/form_question.rb
@@ -2,4 +2,14 @@ class FormQuestion < ApplicationRecord
   belongs_to :form
   belongs_to :question
   belongs_to :response_set
+
+  after_delete do |fq|
+    UpdateIndexJob.perform_async('question', ESQuestionSerializer.new(fq.question))
+    UpdateIndexJob.perform_async('response_set', ESQuestionSerializer.new(fq.response_set)) if fq.response_set
+  end
+
+  after_save do |fq|
+    UpdateIndexJob.perform_async('question', ESQuestionSerializer.new(fq.question))
+    UpdateIndexJob.perform_async('response_set', ESQuestionSerializer.new(fq.response_set)) if fq.response_set
+  end
 end
