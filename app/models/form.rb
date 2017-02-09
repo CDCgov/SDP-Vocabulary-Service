d88a457f0c80f595146300d7c06eadc20c2cb54d
Andrew Schreiber <schreiaj@gmail.com>
2017-02-09 00:19:23 -0500

    Ran linter and rubocop

--- app/models/form.rb
+++ app/models/form.rb
@@ -13,7 +13,7 @@ class Form < ApplicationRecord
                              uniqueness: { message: 'forms should have different OMB Control Numbers',
                                            unless: proc { |f| f.version > 1 && f.other_versions.map(&:control_number).include?(f.control_number) } }
 
-  accepts_nested_attributes_for :questions, allow_destroy: true                                             
+  accepts_nested_attributes_for :questions, allow_destroy: true
 
   after_commit :index, on: [:create, :update]
   after_commit :delete_index, on: :destroy
