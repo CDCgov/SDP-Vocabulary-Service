1f7368b2ed72036dc4e2b55bbbda910376249597
Andrew Schreiber <schreiaj@gmail.com>
2017-02-09 10:30:02 -0500

    Moved tests to proper place, fixed last linter errors in a way that didn't involve anonymous functions

--- test/frontend/reducers/test_forms.js
+++ test/frontend/reducers/test_forms.js
@@ -3,7 +3,9 @@ import forms from '../../../webpack/reducers/forms_reducer';
 import _ from 'lodash';
 import {
   FETCH_FORM_FULFILLED,
-  FETCH_FORMS_FULFILLED
+  FETCH_FORMS_FULFILLED,
+  ADD_QUESTION,
+  REMOVE_QUESTION
 } from '../../../webpack/actions/types';
 
 describe('forms reducer', () => {
@@ -19,5 +21,23 @@ describe('forms reducer', () => {
     const startState = {}
     const nextState = forms(startState, action);
     expect(Object.keys(nextState).length).to.equal(2);
+  });
+
+  it('should add a question', () => {
+    const form = {id: 1, name: "Red Form",  userId: "testAuthor@gmail.com", formQuestions:[]}
+    const question = {id: 1, content: "Is this a question?", questionType: ""};
+    const action = {type: ADD_QUESTION, payload: {question, form} };
+    const startState = {};
+    const nextState = forms(startState, action);
+    expect(nextState["1"].formQuestions[0].questionId).to.equal(question.id);
+  });
+
+  it('should remove a question', () => {
+    const question = {id: 1, content: "Is this a question?", questionType: ""};
+    const form = {id: 1, name: "Red Form",  userId: "testAuthor@gmail.com", formQuestions:[question]};
+    const action = {type: REMOVE_QUESTION, payload: {form, index: 0} };
+    const startState = {1: form};
+    const nextState = forms(startState, action);
+    expect(nextState["1"].formQuestions.length).to.equal(0);
   });
 });
--- test/frontend/reducers/test_questions.js
+++ test/frontend/reducers/test_questions.js
@@ -19,21 +19,6 @@ describe('questions reducer', () => {
     expect(nextState[1].id).to.equal(question.data.id);
   });
 
-  it('should add a question', () => {
-    const question = {data:{id: 1, content: "Is this a question?", questionType: ""}};
-    const action = {type: ADD_QUESTION, payload: question };
-    const startState = {};
-    const nextState = questions(startState, action);
-    expect(nextState[1].id).to.equal(question.data.id);
-  });
-
-  it('should remove a question', () => {
-    const action = {type: REMOVE_QUESTION, payload: 0 };
-    const startState = {1: {id: 1, content: "Is this a question?", questionType: ""}};
-    const nextState = questions(startState, action);
-    expect(Object.keys(nextState).length).to.equal(0);
-  });
-
   it('should fetch questions', () => {
     const questionData = {data: [{id: 1, content: "Is this a question?", questionType: ""},
                                  {id: 2, content: "Whats your name", questionType: ""},
