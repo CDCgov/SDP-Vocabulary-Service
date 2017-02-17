b8e94e72377aa4b336b667e7dcdf6fe1ca308687
Andy Gregorowicz <andy@mitre.org>
2017-02-17 08:32:38 -0500

    Removing more unused stuff
    Eliminating unused webpack entry points
    Replacing the old application layout with the one we had in landing

--- webpack/search.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SearchWidget from './components/SearchWidget';
-import Routes from "./routes";
-import _ from 'lodash';
-
-const questions = _.keyBy(JSON.parse(document.getElementById('question-json').innerHTML), 'id');
-const responseSets = _.keyBy(JSON.parse(document.getElementById('response-set-json').innerHTML), 'id');
-ReactDOM.render(<SearchWidget questions={questions} responseSets={responseSets} routes={Routes} />, document.getElementById('search-widget'));
