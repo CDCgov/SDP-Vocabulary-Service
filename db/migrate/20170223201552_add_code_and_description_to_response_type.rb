d6f48f7d89abcf949fa36b56b5aee4faf29e350b
Rob Dingwell <rob@robdingwell.org>
2017-02-23 15:57:32 -0500

    adding code and description to response_types , adding HL7 questions types , codes and descriptions

---
+++ db/migrate/20170223201552_add_code_and_description_to_response_type.rb
@@ -0,0 +1,39 @@
+class AddCodeAndDescriptionToResponseType < ActiveRecord::Migration[5.0]
+  def change
+    add_column :response_types, :code, :string
+    add_column :response_types, :description, :text
+
+    types = [{ code: 'decimal', name: 'Decimal',
+               description: 'Answer is a floating point number.' },
+             { code: 'integer', name: 'Integer',
+               description: 'Answer is an integer.' },
+             { code: 'date', name: 'Date',
+               description: 'Answer is a date.' },
+             { code: 'dateTime', name: 'Date Time',
+               description: 'Answer is a date and time.' },
+             { code: 'instant', name: 'Instant',
+               description: 'Answer is a system timestamp.' },
+             { code: 'time', name: 'Time',
+               description: 'Answer is a time (hour/minute/second) independent of date.' },
+             { code: 'string', name: 'String',
+               description: 'Answer is a short (few words to short sentence) free-text entry.' },
+             { code: 'text', name: 'Text',
+               description: 'Answer is a long (potentially multi-paragraph) free-text entry (still captured as a string).' },
+             { code: 'url', name: 'Url',
+               description: 'Answer is a url (website, FTP site, etc.).' },
+             { code: 'choice', name: 'Choice',
+               description: 'Answer is a Coding drawn from a list of options.' },
+             { code: 'open-choice', name: 'Open Choice',
+               description: 'Answer is a Coding drawn from a list of options or a free-text entry.' },
+             { code: 'attachment', name: 'Attachment',
+               description: 'Answer is binary content such as a image, PDF, etc.' },
+             { code: 'reference', name: 'Reference',
+               description: 'Answer is a reference to another resource (practitioner, organization, etc.).' },
+             { code: 'quantity', name: 'Quantity',
+               description: 'Answer is a combination of a numeric value and unit, potentially with a comparator (<, >, etc.).' }]
+    types.each do |type|
+      rtype = ResponseType.find_or_create_by(name: type[:name])
+      rtype.update!(type)
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -136,8 +136,10 @@ ActiveRecord::Schema.define(version: 20170307205339) do
 
   create_table "response_types", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "code"
+    t.text     "description"
   end
 
   create_table "responses", force: :cascade do |t|
