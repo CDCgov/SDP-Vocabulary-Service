6aa27ccfaca6d3ba6814c051bda2a732503ff3d2
Rob Dingwell <rob@robdingwell.org>
2017-03-01 11:50:26 -0500

    adding validations to response set for source attribute and some general validations that should have been there before hand

--- test/models/response_set_test.rb
+++ test/models/response_set_test.rb
@@ -1,6 +1,45 @@
 require 'test_helper'
 
 class ResponseSetTest < ActiveSupport::TestCase
+  setup do
+    @user = users(:admin)
+  end
+
+  test 'validates name' do
+    rs = ResponseSet.new(name: 'Test Set', created_by: @user)
+    assert rs.valid?, 'Response set should be valid with a name'
+
+    rs = ResponseSet.new(created_by: @user)
+    assert !rs.valid?, 'Response set should not be valid without a name'
+  end
+
+  test 'validates created_by' do
+    rs = ResponseSet.new(name: 'Test Set',  created_by: @user)
+    assert rs.valid?, 'Response set should be valid with a created_by user'
+
+    rs = ResponseSet.new(name: 'Test Set')
+    assert !rs.valid?, 'Response set should not be valid without a created_by user'
+  end
+
+  test 'validates  source' do
+    rs = ResponseSet.new(name: 'Test Set',  created_by: @user)
+    assert rs.valid?
+    assert_equal 'local', rs.source, 'default source attribute should be local'
+
+    rs.source = 'PHIN_VADS'
+    assert rs.valid?, 'response set should be valid with PHIN_VADS as source '
+
+    rs.source = nil
+    assert !rs.valid?, 'Response set should not be valid with nil source '
+
+    rs.source = 'NLM VSAC'
+    assert !rs.valid?, 'Response set should not be valid with source something other than local or PHIN_VADS'
+  end
+
+  test 'latest_versions' do
+    assert_equal 2, ResponseSet.latest_versions.count
+  end
+
   test 'build_new_revision' do
     rs = response_sets(:gfv2)
     revision = rs.build_new_revision
@@ -16,13 +55,13 @@ class ResponseSetTest < ActiveSupport::TestCase
   end
 
   test 'assigns a version_independent_id when saved the first time' do
-    rs = ResponseSet.new(name: 'Test Set', version: 1)
+    rs = ResponseSet.new(name: 'Test Set', version: 1, created_by: @user)
     assert rs.save!
     assert_equal "RS-#{rs.id}", rs.version_independent_id
   end
 
   test 'does not let you create a record at a version other than one' do
-    rs = ResponseSet.new(name: 'Test Set', version: 2)
+    rs = ResponseSet.new(name: 'Test Set', version: 2, created_by: @user)
     assert_raise ActiveRecord::RecordInvalid do
       rs.save!
     end
@@ -31,44 +70,49 @@ class ResponseSetTest < ActiveSupport::TestCase
   test 'assign_new_oids' do
     prefix = ResponseSet.oid_prefix
 
-    rs1 = ResponseSet.new
-    assert rs1.save
-    assert_equal 1, rs1.version
-    assert_equal "RS-#{rs1.id}", rs1.version_independent_id
-    assert_equal "#{prefix}.#{rs1.id}", rs1.oid
+    rs = ResponseSet.new(name: 'Test', created_by: @user)
+    rs.id = 3
 
-    rs2 = ResponseSet.new
-    rs2.oid = "#{prefix}.#{rs1.id}"
-    assert_not rs2.valid?
-    rs2.oid = "#{prefix}.#{rs1.id + 1}"
-    assert rs2.valid?
-    assert rs2.save
+    assert rs.save
+    assert_equal 1, rs.version
+    assert_equal 'RS-3', rs.version_independent_id
+    assert_equal "#{prefix}.3", rs.oid
 
-    rs3 = ResponseSet.new
-    rs3.oid = "#{prefix}.#{rs1.id + 3}"
-    assert rs3.save
+    rs = ResponseSet.new(name: 'Test', created_by: @user)
+    rs.oid = "#{prefix}.3"
+    assert_not rs.valid?
+    rs.oid = "#{prefix}.4"
+    assert rs.valid?
+    rs.save
 
-    rs4 = ResponseSet.new
-    rs4.oid = "#{prefix}.#{rs1.id + 5}"
-    assert rs4.save
+    rs = ResponseSet.new(name: 'Test', created_by: @user)
+    rs.id = 4
+    rs.oid = "#{prefix}.6"
+    rs.save
 
-    # Should find next available oid which is rs1.id+4 NOT rs1.id+6
-    rs5 = ResponseSet.new
-    assert rs5.save
-    assert_equal "#{prefix}.#{rs1.id + 4}", rs5.oid
+    rs = ResponseSet.new(name: 'Test', created_by: @user)
+    rs.id = 5
+    rs.oid = "#{prefix}.8"
+    asssert rs.save
+
+    # Should find next available oid which is .7 NOT .9
+    rs = ResponseSet.new(name: 'Test', created_by: @user)
+    rs.id = 6
+    assert rs.save
+    assert_equal "#{prefix}.7", rs.oid
 
     # Should follow special validation rules for new versions
-    rs6 = ResponseSet.new
-    rs6.version_independent_id = rs1.version_independent_id
-    rs6.version = rs1.version + 1
-    assert rs6.save
-    assert_equal rs1.oid, rs6.oid
+    rs2 = ResponseSet.new(name: 'Test', created_by: @user)
+    rs2.version_independent_id = rs.version_independent_id
+    rs2.version = 2
+    rs2.save
+    assert_equal rs.oid, rs2.oid
 
-    rs7 = ResponseSet.new
-    rs7.version_independent_id = rs1.version_independent_id
-    rs7.version = rs1.version + 2
-    rs7.oid = rs1.oid
-    assert rs7.save
-    assert_equal rs1.oid, rs7.oid
+    rs3 = ResponseSet.new(name: 'Test', created_by: @user)
+    rs3.version_independent_id = rs.version_independent_id
+    rs3.version = 3
+    rs3.oid = rs.oid
+    assert rs3.save
+    assert_equal rs.oid, rs3.oid
   end
 end
