14b547846aa038ddadafa71469ffcbc32a2bb377
Andy Gregorowicz <andy@mitre.org>
2017-03-08 11:38:03 -0500

    Yet again regenerating the routes

--- webpack/_routes.js
+++ webpack/_routes.js
@@ -601,6 +601,9 @@ Based on Rails routes of Vocabulary::Application
 // publish_question => /questions/:id/publish(.:format)
   // function(id, options)
   publish_question_path: Utils.route([["id",true],["format",false]], {}, [2,[7,"/",false],[2,[6,"questions",false],[2,[7,"/",false],[2,[3,"id",false],[2,[7,"/",false],[2,[6,"publish",false],[1,[2,[8,".",false],[3,"format",false]],false]]]]]]]),
+// publish_response_set => /response_sets/:id/publish(.:format)
+  // function(id, options)
+  publish_response_set_path: Utils.route([["id",true],["format",false]], {}, [2,[7,"/",false],[2,[6,"response_sets",false],[2,[7,"/",false],[2,[3,"id",false],[2,[7,"/",false],[2,[6,"publish",false],[1,[2,[8,".",false],[3,"format",false]],false]]]]]]]),
 // question => /questions/:id(.:format)
   // function(id, options)
   question_path: Utils.route([["id",true],["format",false]], {}, [2,[7,"/",false],[2,[6,"questions",false],[2,[7,"/",false],[2,[3,"id",false],[1,[2,[8,".",false],[3,"format",false]],false]]]]]),
