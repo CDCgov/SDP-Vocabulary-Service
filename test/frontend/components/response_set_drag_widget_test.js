d0cb278e234f3a18b39999296bb600f39c0e13ba
John Mazella <jmazella@mitre.org>
2017-03-09 15:56:36 -0500

    Fixing another test

--- test/frontend/components/response_set_drag_widget_test.js
+++ test/frontend/components/response_set_drag_widget_test.js
@@ -1,25 +1,12 @@
 import { expect, renderComponent, createComponent } from '../test_helper';
 import TestUtils from 'react-addons-test-utils';
 import ResponseSetDragWidget from '../../../webpack/components/ResponseSetDragWidget';
-import routes from '../mock_routes';
 import MockRouter from '../mock_router';
 
 describe('ResponseSetDragWidget', () => {
   let component, router, inputNode, props;
 
   beforeEach(() => {
-    router = new MockRouter();
-    props  = {
-      question: {id: 1, content: "Is this a question?", questionType: "", responseSets: [1], concepts: [{code:"Code 1", display:" Display Name 1", system:"Test system 1"}]},
-      route: {},
-      router: router,
-      routes: routes,
-      questionSubmitter: ()=>{},
-      questionTypes: {},
-      responseSets:  {1: {id: 1, name: "Colors", description: "A list of colors", oid: "2.16.840.1.113883.3.1502.3.1"}},
-      responseTypes: {}
-    };
-
     props = { handleResponseSetsChange: ()=>{},
               responseSets: {1: {id: 1, name: "Colors", description: "A list of colors", oid: "2.16.840.1.113883.3.1502.3.1"}, 
                              2: {id: 2, name: "test", description: "A list of colors", oid: "2.16.840.1.113883.3.1502.3.2"}},
