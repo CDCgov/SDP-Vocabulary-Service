e0206a1848de3c35bfd9c819aac20898d5ef4e4e
Rob Dingwell <rob@robdingwell.org>
2017-03-16 18:23:47 -0400

    fixing issues with features using search methods incorrectly

--- features/step_definitions/form_steps.rb
+++ features/step_definitions/form_steps.rb
@@ -13,7 +13,7 @@ When(/^I go to the list of Forms$/) do
 end
 
 When(/^I click on the button to add the Question "([^"]*)"$/) do |question_content|
-  object_id = Question.search(question_content).first.id.to_s
+  object_id = Question.find_by(content: question_content).id.to_s
   page.find("#question_#{object_id}_add").click
 end
 
@@ -24,7 +24,6 @@ end
 
 When(/^I move the Question "([^"]*)" (up|down)$/) do |question_content, direction|
   object_id = attribute_to_id('Question', 'content', question_content)
-
   old_index = page.find_all('.question-item').index { |el| el.has_css?("#question_id_#{object_id}") }
 
   page.find("#question_id_#{object_id}").find(:xpath, '../../../..').find(".move-#{direction}").click
