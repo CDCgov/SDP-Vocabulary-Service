5ec968726756e9b0195d75996683c29f7383e80d
Eny Hathaway <ehathaway@mitre.org>
2017-03-20 18:01:51 -0400

    adding structure for landing page

---
+++ webpack/containers/Home.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+class Home extends Component {
+
+  render() {
+    return (
+      <div className="site-wrapper">
+
+      <div className="site-wrapper-inner">
+
+        <div className="cover-container">
+
+      
+          <div className="inner cover">
+            <h1 className="cover-heading">CDC Vocabulary Service</h1>
+            <p className="lead">Add home page description</p>
+            <p className="lead">
+              <a href="#" className="btn btn-lg btn-default">Log In</a>
+            </p>
+            
+            
+          </div>
+
+          <div className="mastfoot">
+            <div className="inner">
+              <p>Cover template for <a href="http://getbootstrap.com">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>    );
+  }
+}
+
+export default Home;
+
