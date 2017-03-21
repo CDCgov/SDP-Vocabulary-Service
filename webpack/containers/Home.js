db4d0db43a3b9d054ab8475f91dc37c124de7244
Eny Hathaway <ehathaway@mitre.org>
2017-03-20 22:48:09 -0400

    adding landing page html

--- webpack/containers/Home.js
+++ webpack/containers/Home.js
@@ -12,9 +12,9 @@ class Home extends Component {
       
           <div className="inner cover">
             <h1 className="cover-heading">CDC Vocabulary Service</h1>
-            <p className="lead">Add home page description</p>
+            <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer elit sapien, dictum a volutpat eu, rutrum egestas sem. Proin lorem justo, placerat id eleifend eu, dictum sed magna. </p>
             <p className="lead">
-              <a href="#" className="btn btn-lg btn-default">Log In</a>
+              <a href="#" className="btn btn-lg btn-default">Login</a>
             </p>
             
             
@@ -22,7 +22,7 @@ class Home extends Component {
 
           <div className="mastfoot">
             <div className="inner">
-              <p>Cover template for <a href="http://getbootstrap.com">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+            <p> 2016 Centers for Disease Control and Prevention. All rights reserved. Privacy Security Terms of Service</p>	
             </div>
           </div>
 
@@ -30,7 +30,8 @@ class Home extends Component {
 
       </div>
 
-    </div>    );
+    </div>    
+    );
   }
 }
 
