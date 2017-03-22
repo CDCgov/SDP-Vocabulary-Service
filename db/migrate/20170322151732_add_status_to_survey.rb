325ca51202374c3235860a94b7591df9d2bf85c2
Rob Dingwell <rob@robdingwell.org>
2017-03-22 11:28:17 -0400

    fixing issue with migration and updating erd

--- db/migrate/20170322151732_add_status_to_survey.rb
+++ db/migrate/20170322151732_add_status_to_survey.rb
@@ -1,5 +1,5 @@
 class AddStatusToSurvey < ActiveRecord::Migration[5.0]
   def change
-    add_column :surveys, :status, :sting, default: 'draft'
+    add_column :surveys, :status, :string, default: 'draft'
   end
 end
