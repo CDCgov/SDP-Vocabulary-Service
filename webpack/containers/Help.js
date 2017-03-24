99251277497f99eeacb1ce71f710c38142452a53
Eny Hathaway <ehathaway@mitre.org>
2017-03-24 06:30:46 -0400

    added tabs to help page

--- webpack/containers/Help.js
+++ webpack/containers/Help.js
@@ -15,17 +15,38 @@ class Help extends Component {
 				
 		<div className="container">
 			<div className="row">
-				<div className="col-md-3">Menu: FAQs, Glossary, Release Notes
-				
+				<div className="col-md-12">
+            
+            <ul className="nav nav-tabs" role="tablist">
+              <li className="nav-item">
+                <a className="nav-link active" data-toggle="tab" href="#faq" role="tab">FAQs</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" data-toggle="tab" href="#glossary" role="tab">Glossary</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" data-toggle="tab" href="#releasenotes" role="tab">Release Notes</a>
+              </li>
+            </ul>
+            
+            <div className="tab-content">
+              <div className="tab-pane active" id="faq" role="tabpanel">
+                <h1>FAQs</h1>
+                <p>content</p>
+              
+              </div>
+              <div className="tab-pane" id="glossary" role="tabpanel">
+                <h1>Glossary</h1>
+                <p>content</p>
+              </div>
+              <div className="tab-pane" id="releasenotes" role="tabpanel">
+                <h1>Release Notes</h1>
+                <p>content</p>
+              </div>
+            </div>
 				
 				</div>
-				
-				<div className="col-md-9">Content
-				
-				
-				</div>
-			
-			
+							
 			</div>
 		</div>		
 	
