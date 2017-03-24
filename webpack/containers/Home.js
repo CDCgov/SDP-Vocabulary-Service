f0ac01d47c3ad1916bce087456cb3c7f371afae6
Eny Hathaway <ehathaway@mitre.org>
2017-03-24 07:45:27 -0400

    updating landing page layout and adding content

--- webpack/containers/Home.js
+++ webpack/containers/Home.js
@@ -12,12 +12,24 @@ class Home extends Component {
       
           <div className="inner cover">
             <h1 className="cover-heading">CDC Vocabulary Service</h1>
-            <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer elit sapien, dictum a volutpat eu, rutrum egestas sem. Proin lorem justo, placerat id eleifend eu, dictum sed magna. </p>
+            
+            <h3 className="tagline">Author Questions, Response Sets, and Forms</h3>
+
+            <p className="lead">
+
+          This service aims to make it easier for users to find out how other groups have asked for certain types of information, and reuse this wording for their own data collection needs. </p>
             <p className="lead">
               <a href="#" className="btn btn-lg btn-default">Login</a>
             </p>
             
-            
+          </div>
+          
+          <div className="cover-containe">
+            <ul className="list-inline">
+              <li>Author Content</li>
+              <li>Collaborate</li>
+              <li>Build</li>
+            </ul>
           </div>
 
           <div className="mastfoot">
