a8ae57e7389e2853e93dbb515eda230ce54bf1d6
Andrew Schreiber <schreiaj@gmail.com>
2017-04-04 16:49:35 -0400

    Added handling for custom error pages on the React side

--- webpack/middleware/unauthenticated_response.js
+++ webpack/middleware/unauthenticated_response.js
@@ -1,14 +1,23 @@
 import {
-  LOG_OUT
+  LOG_OUT,
+  HANDLE_ERROR
 } from '../actions/types';
+import { hashHistory } from 'react-router';
 
 const parentFromQuestions = store => next => action => {
   let {payload} = action;
-  if(payload && payload.response && payload.response.status == 401) {
-    // If we are getting 401s we know the session on the server is expired
-    store.dispatch({
-      type: LOG_OUT
-    });
+  if(payload && payload.response) {
+    switch (payload.response.status) {
+      case 401:
+        // If we are getting 401s we know the session on the server is expired
+        store.dispatch({
+          type: LOG_OUT
+        });
+        break;
+      default:
+        hashHistory.push(`/errors/${payload.response.status}`);
+    }
+
   }
   next(action);
 };
