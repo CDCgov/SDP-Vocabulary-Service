6f4e982fbd24b8dc36b57b9cbb0d7390412054af
Andrew Pellitieri <apellitieri@mitre.org>
2017-04-05 17:02:43 -0400

    Testing search bar working with ES on form edit

--- webpack/containers/QuestionSearchContainer.js
+++ webpack/containers/QuestionSearchContainer.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import { addQuestion } from '../actions/questions_actions';
+import { fetchSearchResults, fetchMoreSearchResults } from '../actions/search_results_actions';
 
+import SearchResult from '../components/SearchResult';
+import DashboardSearch from '../components/DashboardSearch';
+import currentUserProps from "../prop-types/current_user_props";
 import QuestionResults from '../components/QuestionResults';
 import SearchBar from '../components/SearchBar';
 import _ from 'lodash';
@@ -11,12 +15,19 @@ import _ from 'lodash';
 class QuestionSearchContainer extends Component {
   constructor(props) {
     super(props);
-
+    this.search = this.search.bind(this);
+    this.loadMore = this.loadMore.bind(this);
     this.state = {
       questions: props.allQs,
       responseSets: props.allRs,
-      allQs: props.allQs
+      allQs: props.allQs,
+      searchTerms: '',
+      page: 1
     };
+  }
+
+  componentWillMount() {
+    this.search('');
   }
 
   componentWillUpdate(prevProps) {
@@ -45,21 +56,47 @@ class QuestionSearchContainer extends Component {
     });
   }
 
+  search(searchTerms) {
+    if(searchTerms === ''){
+      searchTerms = null;
+    }
+    this.setState({searchTerms: searchTerms});
+    this.props.fetchSearchResults(searchTerms, 'question');
+  }
+
+  loadMore() {
+    let searchTerms = this.state.searchTerms;
+    let tempState = this.state.page + 1;
+    if(this.state.searchTerms === '') {
+      searchTerms = null;
+    }
+    this.props.fetchMoreSearchResults(searchTerms, 'question', tempState);
+    this.setState({page: tempState});
+  }
+
   render() {
     return (
-            <div>
-                <SearchBar modelName='question' onSearchTermChange={term => this.questionFilter(term)} />
-                <QuestionResults questions={this.props.reverseSort ? this.state.questions.reverse() : this.state.questions}
-                                 responseSets={_.values(this.state.responseSets)}
-                                 addQuestion={this.props.addQuestion}
-                                 form={this.props.form} />
-            </div>
+      <div>
+        <DashboardSearch search={this.search} />
+        <SearchBar modelName='question' onSearchTermChange={term => this.questionFilter(term)} />
+        <QuestionResults questions={this.props.reverseSort ? this.state.questions.reverse() : this.state.questions}
+                         responseSets={_.values(this.state.responseSets)}
+                         addQuestion={this.props.addQuestion}
+                         form={this.props.form} />
+      </div>
     );
   }
 }
 
+function mapStateToProps(state) {
+  return {
+    searchResults: state.searchResults,
+    currentUser: state.currentUser
+  };
+}
+
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({addQuestion}, dispatch);
+  return bindActionCreators({addQuestion, fetchSearchResults, fetchMoreSearchResults}, dispatch);
 }
 
 QuestionSearchContainer.propTypes = {
@@ -67,7 +104,11 @@ QuestionSearchContainer.propTypes = {
   allRs: React.PropTypes.array,
   addQuestion: React.PropTypes.func.isRequired,
   form: React.PropTypes.object,
-  reverseSort: React.PropTypes.bool
+  reverseSort: React.PropTypes.bool,
+  fetchSearchResults: PropTypes.func,
+  fetchMoreSearchResults: PropTypes.func,
+  currentUser: currentUserProps,
+  searchResults: PropTypes.object
 };
 
-export default connect(null, mapDispatchToProps)(QuestionSearchContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(QuestionSearchContainer);
