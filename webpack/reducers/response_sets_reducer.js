9105c032917411edc9daf046a4f3957a08394370
Andy Gregorowicz <andy@mitre.org>
2017-04-05 20:34:05 -0400

    Handle response set usage showing up before fetch

--- webpack/reducers/response_sets_reducer.js
+++ webpack/reducers/response_sets_reducer.js
@@ -19,14 +19,21 @@ export default function responseSets(state = {}, action) {
     case PUBLISH_RESPONSE_SET_FULFILLED:
     case SAVE_RESPONSE_SET_FULFILLED:
       responseSetClone = Object.assign({}, state);
-      responseSetClone[action.payload.data.id] = action.payload.data;
+      const id = action.payload.data.id;
+      const existingRs = responseSetClone[action.payload.data.id];
+      if (existingRs) {
+        responseSetClone[id] = _.assign(existingRs, action.payload.data);
+      } else {
+        responseSetClone[id] = action.payload.data;
+      }
       return responseSetClone;
     case FETCH_RESPONSE_SET_USAGE_FULFILLED:
       responseSetClone = Object.assign({}, state);
-      if (responseSetClone[action.payload.data.id]) {
-        responseSetClone[action.payload.data.id].surveillancePrograms = action.payload.data.surveillancePrograms;
-        responseSetClone[action.payload.data.id].surveillanceSystems = action.payload.data.surveillanceSystems;
+      if (responseSetClone[action.payload.data.id] === undefined) {
+        responseSetClone[action.payload.data.id] = {};
       }
+      responseSetClone[action.payload.data.id].surveillancePrograms = action.payload.data.surveillancePrograms;
+      responseSetClone[action.payload.data.id].surveillanceSystems = action.payload.data.surveillanceSystems;
       return responseSetClone;
     default:
       return state;
