bd2577dd3bfd96ed1f3eeffb9d32b1b285d191b6
Andrew Hubley <ahubley@mitre.org>
2017-04-20 13:35:52 -0400

    Added Program Defined Variable Name
    Adjusted API swagger doc accordingly
    Updated fixtures and schema accordingly

--- app/serializers/form_questions_serializer.rb
+++ app/serializers/form_questions_serializer.rb
@@ -1,6 +1,7 @@
 class FormQuestionsSerializer < ActiveModel::Serializer
   attribute :question
   attribute :response_set, key: :response
+  attribute :program_var
 
   def question
     QuestionsSerializer.new(object.question)
