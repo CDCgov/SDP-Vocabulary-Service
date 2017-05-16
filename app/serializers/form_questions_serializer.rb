ae14163a9389779f3b0afd23c8c73f6f83c98ee0
Andrew Hubley <ahubley@mitre.org>
2017-05-16 16:57:01 -0400

    Updated Serializers to use blocks
    Instead of methods for API fields

--- app/serializers/form_questions_serializer.rb
+++ app/serializers/form_questions_serializer.rb
@@ -1,13 +1,9 @@
 class FormQuestionsSerializer < ActiveModel::Serializer
-  attribute :question
-  attribute :response_set, key: :response
-  attribute :program_var
-
-  def question
+  attribute :question do
     QuestionsSerializer.new(object.question)
   end
-
-  def response_set
+  attribute :response do
     ValueSetsSerializer.new(object.response_set) if object.response_set
   end
+  attribute :program_var
 end
--- app/serializers/questions_serializer.rb
+++ app/serializers/questions_serializer.rb
@@ -3,14 +3,12 @@ include ActiveModel::Serialization
 class QuestionsSerializer < ActiveModel::Serializer
   attribute :version_independent_id, key: :questionId
   attribute :content, key: :questionText
-  attribute :question_uri, key: :questionUri
-  def question_uri
+  attribute :questionUri do
     Rails.application.routes.url_helpers.api_question_url(object.version_independent_id, version: object.version, only_path: true)
   end
   attribute :version, key: :version
   attribute :published_by, serializer: UserSerializer
-  attribute :question_type, key: :questionType
-  def question_type
+  attribute :questionType do
     object.question_type.name
   end
 end
