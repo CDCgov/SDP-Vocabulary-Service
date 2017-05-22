14c7bb59b017ea5549b9484c134e2b2f70e31835
Rob Dingwell <rob@robdingwell.org>
2017-05-22 09:27:44 -0400

    adding back in the disabling of registration routes when user registration is disabled

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,14 @@ class User < ApplicationRecord
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:openid_connect]
+  if DISABLE_USER_REGISTRATION
+    devise :database_authenticatable,
+           :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:openid_connect]
+  else
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:openid_connect]
 
+  end
   validates :email, uniqueness: true, case_sensitive: false
   has_many :authentications
 
