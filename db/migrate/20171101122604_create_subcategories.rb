442eb3768fa17f7f7654a83be8d2cf7d37e6e91a
Andrew Pellitieri <apellitieri@mitre.org>
2017-11-09 10:55:01 -0500

    Fixing migration issue

--- db/migrate/20171101122604_create_subcategories.rb
+++ db/migrate/20171101122604_create_subcategories.rb
@@ -15,16 +15,16 @@ class CreateSubcategories < ActiveRecord::Migration[5.1]
       ep = QuestionType.find_or_create_by name: 'Emergency Preparedness'
       epid = QuestionType.find_or_create_by name: 'Epidemiological'
 
-      Subcategory.find_or_create_by name: 'Managing & Commanding', question_type: ep
-      Subcategory.find_or_create_by name: 'Operations', question_type: ep
-      Subcategory.find_or_create_by name: 'Planning/Intelligence', question_type: ep
-      Subcategory.find_or_create_by name: 'Logistics', question_type: ep
-      Subcategory.find_or_create_by name: 'Financial/Administration', question_type: ep
+      Subcategory.find_or_create_by name: 'Managing & Commanding', question_type_id: ep.id
+      Subcategory.find_or_create_by name: 'Operations', question_type_id: ep.id
+      Subcategory.find_or_create_by name: 'Planning/Intelligence', question_type_id: ep.id
+      Subcategory.find_or_create_by name: 'Logistics', question_type_id: ep.id
+      Subcategory.find_or_create_by name: 'Financial/Administration', question_type_id: ep.id
 
-      Subcategory.find_or_create_by name: 'Travel', question_type: epid
-      Subcategory.find_or_create_by name: 'Contact or Exposure', question_type: epid
-      Subcategory.find_or_create_by name: 'Drug Abuse', question_type: epid
-      Subcategory.find_or_create_by name: 'Sexual Behavior', question_type: epid
+      Subcategory.find_or_create_by name: 'Travel', question_type_id: epid.id
+      Subcategory.find_or_create_by name: 'Contact or Exposure', question_type_id: epid.id
+      Subcategory.find_or_create_by name: 'Drug Abuse', question_type_id: epid.id
+      Subcategory.find_or_create_by name: 'Sexual Behavior', question_type_id: epid.id
     end
   end
 end
