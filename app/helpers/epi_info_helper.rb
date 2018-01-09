fb0d81f3428321c5d94557ca6283f0ae7be6ef75
Rob Dingwell <rob@robdingwell.org>
2018-01-09 11:34:53 -0500

    WIP: adding epi info field type mapping

---
+++ app/helpers/epi_info_helper.rb
@@ -0,0 +1,34 @@
+module EpiInfoHelper
+  def epi_info_field_type(code)
+    case code
+    when 'decimal'
+      5
+    when 'integer'
+      5
+    when 'date'
+      7
+    when 'dateTime'
+      9
+    when 'instant'
+      8
+    when 'time'
+      8
+    when 'string'
+      1
+    when 'text'
+      4
+    when 'url'
+      1
+    when 'choice'
+      18
+    when 'open-choice'
+      18
+    when 'attachment'
+      99
+    when 'quantity'
+      1
+    else
+      99
+    end
+  end
+end
