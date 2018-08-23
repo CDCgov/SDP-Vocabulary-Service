c82f0a2390a431347b480c3e608afba45f3c101b
Andrew Pellitieri <apellitieri@mitre.org>
2018-08-23 11:53:30 -0400

    Updated the packages to allow for npm audit and fixed vads vs sync bugs

--- test/frontend/test_helper.js
+++ test/frontend/test_helper.js
@@ -10,6 +10,7 @@ import chaiJquery from 'chai-jquery';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../../webpack/reducers';
+import 'react-dates/initialize';
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
