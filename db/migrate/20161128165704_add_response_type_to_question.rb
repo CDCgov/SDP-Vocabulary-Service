a496c1e2025a5060b5190a7c48cbfb2616cb415c
Andrew Pellitieri <apellitieri@mitre.org>
2018-09-06 10:31:19 -0400

    Fixing import of N/A rs and count calculations

--- db/migrate/20161128165704_add_response_type_to_question.rb
+++ db/migrate/20161128165704_add_response_type_to_question.rb
@@ -1,6 +1,7 @@
 class AddResponseTypeToQuestion < ActiveRecord::Migration[5.0]
   def change
     add_reference :questions, :response_type, foreign_key: true
-    Question.update_all response_type_id: ResponseType.find_by(name: 'Response Set')
+    rt = ResponseType.find_or_create_by(code: 'choice', name: 'Choice', description: 'Answer is a Coding drawn from a list of options.')
+    Question.update_all response_type_id: rt.id if rt
   end
 end
--- db/migrate/20161212180833_add_versions_to_forms.rb
+++ db/migrate/20161212180833_add_versions_to_forms.rb
@@ -2,10 +2,6 @@ class AddVersionsToForms < ActiveRecord::Migration[5.0]
   def up
     add_column :forms, :version_independent_id, :string
     add_column :forms, :version, :integer, default: 1
-
-    Form.all.each do |f|
-      f.update_attribute :version_independent_id, "F-#{f.id}"
-    end
   end
 
   def down
