d5646e6fb18d981f7d8332981e6c106ee66c5fc3
Chris Sandlin <zoo3@cdc.gov>
2018-11-30 17:34:20 -0500

    added oid parameter

--- app/controllers/api/response_sets_controller.rb
+++ app/controllers/api/response_sets_controller.rb
@@ -3,6 +3,9 @@ module Api
     respond_to :json
 
     def index
+      if params[:oid]
+        @value_sets = [ResponseSet.find_by(oid: params[:oid])]
+      else
       @value_sets = params[:search] ? ResponseSet.search(params[:search]) : ResponseSet.all.includes(:responses, :published_by)
       current_user_id = current_user ? current_user.id : -1
       @value_sets = if params[:limit] && (params[:limit].to_i < 100 || request.env['HTTP_ACCEPT_ENCODING'] == 'gzip')
@@ -10,7 +13,14 @@ module Api
                     else
                       @value_sets.limit(100).where("(status='published' OR created_by_id= ?)", current_user_id)
                     end
+
       @value_sets = @value_sets.order(version_independent_id: :asc)
+    end
+
+    if @value_sets.compact.empty?
+        not_found
+        return
+      end
       render json: @value_sets, each_serializer: ValueSetsSerializer
     end
 
