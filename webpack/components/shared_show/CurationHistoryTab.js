086975defebb47f5dbd887d97c724f1ed3996f97
Chris Sandlin <zoo3@cdc.gov>
2019-03-07 07:37:21 -0500

    created for curation history tab addition

--- webpack/components/shared_show/CurationHistoryTab.js
+++ webpack/components/shared_show/CurationHistoryTab.js
@@ -33,6 +33,18 @@ function GetDuplicateLink(sEvent, nDuplicateOf) {
   }
 }
 
+function GetSuggestedRepOf(sEvent, sSuggestedReplacementOf) {
+  switch(sEvent)
+  {
+    case 'Response Set':
+      return sSuggestedReplacementOf.trim().split(" ").map((replace_id)=>{return(<u>{<Link to={`/responseSets/${replace_id}`}>Response Set #{replace_id}</Link>}<br/></u>);})
+    case 'Question':
+      return sSuggestedReplacementOf.trim().split(" ").map((replace_id)=>{return(<u>{<Link to={`/questions/${replace_id}`}>Question #{replace_id}</Link>}<br/></u>);})
+    default:
+      return 'Suggested Replacement not found.';
+  }
+}
+
     return (
             <div className={`basic-c-box panel-default ${this.props.type}-type`}>
               <div className="panel-heading">
@@ -47,15 +59,13 @@ function GetDuplicateLink(sEvent, nDuplicateOf) {
                     {this.props.duplicateOf && this.props.contentStage && this.props.contentStage === 'Duplicate' &&
                       <div className="box-content">
                         {GetDuplicateLink(this.props.objSetName, this.props.duplicateOf)}
-                        <br/>
-                      </div>
-                    }
+                      </div>}
                 </div>
                 <div className="box-content">
-                  This {this.props.objSetName} has been selected by authors as a replacement for the following {this.props.objSetName}s:
+                    This {this.props.objSetName} has been selected by authors as a replacement for the following {this.props.objSetName}s:
                   <br/>
                   <br/>
-                  <u>PlaceHolder for Link.</u>
+                  {this.props.suggestedReplacementOf && this.props.objSetName && GetSuggestedRepOf(this.props.objSetName, this.props.suggestedReplacementOf)}
                 </div>
             </div>
         );
@@ -64,6 +74,7 @@ function GetDuplicateLink(sEvent, nDuplicateOf) {
 
 CurationHistoryTab.propTypes = {
   duplicateOf: PropTypes.number,
+  suggestedreplacementOf: PropTypes.number,
   contentStage: PropTypes.string,
   type: PropTypes.string
 };
