800fe373576a310fc8ca4e5b5f7cbfd7962ea25a
cssandlin <zoo3@cdc.gov>
2019-09-30 22:17:54 -0400

    updated controller with response stream

--- app/controllers/import_sessions_controller.rb
+++ app/controllers/import_sessions_controller.rb
@@ -2,7 +2,14 @@ class ImportSessionsController < ApplicationController
   authorize_resource
   load_resource only: :update
 
-  def create
+  def index
+    response.headers['Content-Type'] = 'text/html'
+    response.stream.write ' '
+  ensure
+    response.stream.close
+  end
+
+    def create
     render status: :bad_request, json: { error: 'No spreadsheet present' } if import_session_params[:file].blank?
     spreadsheet_upload = import_session_params[:file]
     @import_type = import_session_params[:import_type]
@@ -29,6 +36,8 @@ class ImportSessionsController < ApplicationController
     if @import_session.request_survey_creation && @import_session.top_level_sections > 0
       @import_session.create_survey!(import_type)
     end
+    # writes a bit stream to keep the connection alive
+    index
     render json: @import_session, status: :ok
   end
 
